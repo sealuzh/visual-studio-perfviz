7fe3f7dd3dfa89f5161a9953f444117c8bfd7ca1
RRO <raphael.rosenast@uzh.ch>
2018-01-12 03:17:10 +0100

    Updating Filter Window

--- Client/VSIX-InSituVisualization/Views/PerformanceInfoFilterWindowControl.xaml.cs
+++ Client/VSIX-InSituVisualization/Views/PerformanceInfoFilterWindowControl.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace VSIX_InSituVisualization.Views
 {
-    using System.Diagnostics.CodeAnalysis;
-    using System.Windows;
-
     /// <summary>
     /// Interaction logic for PerformanceInfoFilterWindowControl.
     /// </summary>
@@ -14,20 +11,6 @@
         public PerformanceInfoFilterWindowControl()
         {
             this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "PerformanceInfoFilterWindow");
         }
     }
 }
